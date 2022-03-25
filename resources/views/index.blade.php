8cce7d5b95ea5eca26d8bdd24d73154a5c6197e6
h-emma <emmah92@hotmail.com>
2022-03-25 14:16:11 +0100

    Add button for help and bankId

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@include('header')
+<h1>KOLLEKTIVA</h1>
+<img src="../assets/images/houses.jpg" alt="houses" width="370px" height="500px"/>
+<button class="help">Hjälp</button>
+<button class="bankId">Logga in med BankId</button>
+<p>Saknar du Bank-ID? <a href="#"> Registrera ett Bank-ID här</a></p>
+
+@include('footer')
+
