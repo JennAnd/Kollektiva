9c4dee76ea27e67e0a34471c0492ba4319158d45
h-emma <emmah92@hotmail.com>
2022-03-29 13:28:13 +0200

    WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,16 @@ require("./bootstrap");
 const toggleMenu = document.querySelector(".menu button");
 const closeButton = document.querySelector(".close-button");
 const menu = document.querySelector(".hamburger-card");
+const openMenu = document.getElementById("open-menu");
 toggleMenu.addEventListener("click", function () {
     const open = JSON.parse(toggleMenu.getAttribute("aria-expanded"));
     toggleMenu.setAttribute("aria-expanded", !open);
     menu.hidden = !menu.hidden;
+    if (open) {
+        openMenu.style.display = "block";
+    } else {
+        openMenu.style.display = "none";
+    }
 });
 closeButton.addEventListener("click", function () {
     const open = JSON.parse(toggleMenu.getAttribute("aria-expanded"));
